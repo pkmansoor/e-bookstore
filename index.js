8bc02e150da287e32d598aedebc96ed05381aad0
Mohammed Mansoor P K <pkmansoor@proton.me>
2024-04-20 16:44:37 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3500
+const cors = require('cors')
+
+//middleware
+app.use(cors())
+app.use(express.json( ))
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
